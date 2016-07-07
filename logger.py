63210d32a594f119591bd2849ea79715c0bf6a2e
David Bindel <bindel@cs.cornell.edu>
2016-07-06 22:43:00 -0400

    More clean-up.

--- logger.py
+++ logger.py
@@ -14,11 +14,12 @@ Arguments:
   TITLE    Task description with any tags
 
 Options:
-  -f FILE, --file=FILE    Log file name
-  -p TIME, --prev=TIME    Minutes elapsed since start
-  -a DATE, --after=DATE   Start date of list range
-  -b DATE, --before=DATE  End date of list range
-  -t, --today             Add today's date stamp to title
+  -f FILE, --file=FILE       Log file name
+  -p TIME, --prev=TIME       Minutes elapsed since start
+  -a DATE, --after=DATE      Start date of list range
+  -b DATE, --before=DATE     End date of list range
+  -y DAYS, --yesterday=DAYS  Use date stamp from DAYS ago
+  -t, --today                Add today's date stamp to title
 """
 
 from docopt import docopt
@@ -84,15 +85,11 @@ class RecPrinter(object):
       clock_fmt: Format for elapsed time clock
     """
 
-    def __init__(self, verbose=False):
-        self.verbose = verbose
+    def __init__(self):
         self.style = ansi_codes.copy()
         self.fmt = "{cyan}{date}{plain} {desc}{tags}"
         self.tag_fmt = "{yellow}+{0}{plain}"
-        if verbose:
-            self.clock_fmt = " {brown}[{clock}]{plain}"
-        else:
-            self.clock_fmt = ""
+        self.clock_fmt = " {brown}[{clock}]{plain}"
 
     def _tag_string(self, rec):
         "Render record tags as a string."
@@ -101,24 +98,25 @@ class RecPrinter(object):
         tags = [self.tag_fmt.format(tag, **self.style) for tag in rec['tags']]
         return " " + " ".join(tags)
 
-    def render(self, rec):
+    def render(self, rec, verbose=False):
         "Render record as a string."
         args = self.style.copy()
         args.update(rec)
         args['tags'] = self._tag_string(rec)
         recs = self.fmt.format(**args)
-        if 'tfinish' in rec and 'tstamp' in rec:
+        if verbose and 'tfinish' in rec and 'tstamp' in rec:
             tdiff = rec['tfinish']-rec['tstamp']
             args['clock'] = timedelta(seconds=tdiff.seconds)
             recs += self.clock_fmt.format(**args)
-        if self.verbose and 'note' in rec:
-            for line in rec['note'].splitlines():
-                recs += "\n  {0}".format(line)
+        if verbose and 'note' in rec:
+            if 'note' in rec:
+                for line in rec['note'].splitlines():
+                    recs += "\n  {0}".format(line)
         return recs
 
-    def print(self, rec):
+    def print(self, rec, verbose=False):
         "Print rendered record."
-        print(self.render(rec))
+        print(self.render(rec, verbose=verbose))
 
 
 # ==================================================================
@@ -164,28 +162,41 @@ def has_open_clock(rec):
 
 
 class Logger(object):
+    """Manage a log file.
 
-    def __init__(self, ifname):
+    Provides some basic functionality to load/save a log file,
+    add records, and report on log contents.
+
+    Attributes:
+      printer: RecPrinter object used for output
+      recs: Record list
+    """
+
+    def __init__(self, ifname, printer=None):
+        "Load a log file with the given name."
+        self.printer = printer or RecPrinter()
         with open(ifname, 'rt') as f:
-            self.recs = yaml.load(f)
-        if self.recs is None:
-            self.recs = []
+            self.recs = yaml.load(f) or []
 
     def save(self, ofname=None):
-        if ofname is None:
-            print(yaml.dump(self.recs, default_flow_style=False))
-        else:
-            with open(ofname, 'wt') as f:
-                yaml.dump(self.recs, f, default_flow_style=False)
+        "Write back a log file."
+        with open(ofname, 'wt') as f:
+            yaml.dump(self.recs, f, default_flow_style=False)
+
+    @property
+    def last(self):
+        "Get the last log entry."
+        return self.recs[-1]
 
     def add(self, desc=None, date=None, tags=None):
-        rec = {}
-        self.recs.append(rec)
+        "Add a new record and set the basic fields."
+        self.recs.append({})
         self.update(desc, date, tags)
-        return rec
+        return self.last
 
     def update(self, desc=None, date=None, tags=None):
-        rec = self.recs[-1]
+        "Update record."
+        rec = self.last
         if desc is not None:
             rec['desc'] = desc
         if date is not None:
@@ -194,32 +205,38 @@ class Logger(object):
             rec['tags'] = tags
 
     def start(self, now=None):
-        self.recs[-1]['tstamp'] = now or datetime.now()
+        "Add time stamp to last; if none explicitly given, use current time."
+        self.last['tstamp'] = now or datetime.now()
 
     def finish(self, now=None):
-        self.recs[-1]['tfinish'] = now or datetime.now()
+        "Add finish time to last; if none explicitly given, use current time."
+        self.last['tfinish'] = now or datetime.now()
 
     def elapsed(self, elapsed):
+        "Mark last record as starting elapsed minutes ago (finishing now)."
         now = datetime.now()
         self.start(now-timedelta(minutes=elapsed))
         self.finish(now)
 
     def note(self, note=None):
+        "Add note to last record."
         if note is not None:
-            self.recs[-1]['note'] = note
+            self.last['note'] = note
 
-    def filtered_recs(self, filters):
+    def filtered_recs(self, filters=[]):
+        "Return a filtered list of records."
         recs = self.recs
         for f in filters:
             recs = filter(f, recs)
         return recs
 
     def list(self, filters=[], verbose=True):
-        printer = RecPrinter(verbose=verbose)
+        "Print a filtered list of records."
         for rec in self.filtered_recs(filters):
-            printer.print(rec)
+            self.printer.print(rec, verbose=verbose)
 
     def clock(self, filters=[]):
+        "Compute time spent on a filtered list of records."
         result = timedelta(seconds=0)
         for rec in filter(has_clock, self.filtered_recs(filters)):
             tdiff = rec['tfinish']-rec['tstamp']
@@ -227,12 +244,12 @@ class Logger(object):
         return result
 
     def view(self):
+        "View the last few records."
         print("\nRecent log items")
         print("----------------")
-        printer = RecPrinter(verbose=False)
         recs = self.recs if len(self.recs) <= 5 else self.recs[-5:]
         for rec in recs:
-            printer.print(rec)
+            self.printer.print(rec, verbose=False)
         if self.recs and has_open_clock(self.recs[-1]):
             tdiff = datetime.now() - rec['tstamp']
             tdiff = timedelta(seconds=tdiff.seconds)
@@ -240,46 +257,44 @@ class Logger(object):
 
 
 # ==================================================================
-# Parsing and main routine
+# Parsing date strings and title strings
 
 
 def parse_date(s):
+    "Convert a text date string into a datetime.date"
     dtime = datetime.strptime(s, "%Y-%m-%d")
     return dtime.date()
 
 
 def split_desc(desc=None):
+    "Split a title string into date, description, and tags."
     if desc is None:
+        return (None, None, None)
+    m = re.match('(\d\d\d\d-\d\d-\d\d)(\s*)', desc)
+    if m:
+        date = parse_date(desc[m.start(1):m.end(1)])
+        desc = desc[m.end(2):]
+    else:
         date = None
+    l = desc.split(" +")
+    if len(l[0]) == 0:
         desc = None
         tags = None
+    elif l[0][0] == "+":
+        l[0] = l[0][1:]
+        desc = None
+        tags = l
+    elif len(l) == 1:
+        desc = l[0]
+        tags = None
     else:
-
-        # Split date from front
-        m = re.match('(\d\d\d\d-\d\d-\d\d)(\s*)', desc)
-        if m:
-            date = parse_date(desc[m.start(1):m.end(1)])
-            desc = desc[m.end(2):]
-        else:
-            date = None
-
-        # Split tags from end
-        l = desc.split(" +")
-        if len(l[0]) == 0:
-            desc = None
-            tags = None
-        elif l[0][0] == "+":
-            l[0] = l[0][1:]
-            desc = None
-            tags = l
-        elif len(l) == 1:
-            desc = l[0]
-            tags = None
-        else:
-            desc = l[0]
-            tags = l[1:]
-
+        desc = l[0]
+        tags = l[1:]
     return (desc, tags, date)
+
+
+# ==================================================================
+# Main routine
 
 
 def main():
@@ -287,6 +302,7 @@ def main():
     # Read configuration file
     with open(expanduser('~/.logger.yml'), 'rt') as f:
         default_options = yaml.load(f)
+    printer = RecPrinter()
 
     # Parse options
     options = docopt(__doc__)
@@ -298,13 +314,15 @@ def main():
         fname = default_options['file']
     else:
         fname = 'current.yml'
-    logger = Logger(fname)
+    logger = Logger(fname, printer)
 
     # Split description
     today = datetime.today().date()
     desc, tags, date = split_desc(options['TITLE'])
     if date is None and options['--today']:
         date = today
+    if date is None and options['--yesterday']:
+        date = today-timedelta(days=int(options['--yesterday']))
 
     # Set up any filters
     after  = options['--after']
